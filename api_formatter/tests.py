dc5c47902fd56cc07fd30b23ac7c0d1cc0d89313
Hillel Arnold <helrond@hotmail.com>
2019-08-19 18:09:04 -0400

    adds tests for terms, fixes #13

--- api_formatter/tests.py
+++ api_formatter/tests.py
@@ -7,8 +7,8 @@ from django.urls import reverse
 from elasticsearch_dsl import connections, Search
 from rest_framework.test import APIRequestFactory
 
-from .elasticsearch.documents import Agent
-from .views import AgentViewSet
+from .elasticsearch.documents import Agent, Term
+from .views import AgentViewSet, TermViewSet
 from argo import settings
 
 
@@ -16,10 +16,10 @@ class TestAPI(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
         connections.create_connection(hosts=settings.ELASTICSEARCH_DSL['default']['hosts'], timeout=60)
-        for cls in [Agent]:
+        for cls in [Agent, Term]:
             cls.init()
 
-    def index_fixture_data(self, source_filepath, index, doc_cls):
+    def index_fixture_data(self, source_filepath, doc_cls):
         added_ids = []
         source_filepath = os.path.join(settings.BASE_DIR, source_filepath)
         for f in os.listdir(source_filepath):
@@ -31,16 +31,24 @@ class TestAPI(TestCase):
                 added_ids.append(data['id'])
         return added_ids
 
+    def list_view(self, view, viewset, obj_length):
+        request = self.factory.get(reverse(view))
+        response = viewset.as_view(actions={"get": "list"})(request)
+        self.assertEqual(obj_length, int(response.data['count']))
+
+    def detail_view(self, view, viewset, pk):
+        request = self.factory.get(reverse(view, args=[pk]))
+        response = viewset.as_view(actions={"get": "retrieve"})(request, pk=pk)
+        self.assertEqual(response.status_code, 200)
+
     def test_agents(self):
-        agent_ids = self.index_fixture_data('fixtures/agents', 'agents', Agent)
+        added_ids = self.index_fixture_data('fixtures/agents', Agent)
+        self.list_view('agent-list', AgentViewSet, len(added_ids))
+        for agent_id in added_ids:
+            self.detail_view('agent-detail', AgentViewSet, agent_id)
 
-        # test length of list response
-        request = self.factory.get(reverse('agent-list'))
-        agent_list = AgentViewSet.as_view(actions={"get": "list"})(request)
-        self.assertEqual(len(agent_ids), int(agent_list.data['count']))
-
-        # test individual responses
-        for agent in agent_ids:
-            request = self.factory.get(reverse('agent-detail', args=[agent]))
-            response = AgentViewSet.as_view(actions={"get": "retrieve"})(request, pk=agent)
-            self.assertEqual(response.status_code, 200)
+    def test_terms(self):
+        added_ids = self.index_fixture_data('fixtures/terms', Term)
+        self.list_view('term-list', TermViewSet, len(added_ids))
+        for term_id in added_ids:
+            self.detail_view('term-detail', TermViewSet, term_id)
