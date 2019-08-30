731b0e58ce51955ec2d8f4d36097b4eb0fbc4cb7
Hillel Arnold <helrond@hotmail.com>
2019-08-30 09:58:23 -0400

    improves analyzers, fixes #30

--- api_formatter/elasticsearch/analyzers.py
+++ api_formatter/elasticsearch/analyzers.py
@@ -1,9 +1,7 @@
 from elasticsearch_dsl import analyzer
 
-# TODO: build these out as necessary
-
 html_strip = analyzer(
-    'html_strip',
+    'base_analyzer',
     tokenizer="standard",
     filter=["lowercase", "stop", "snowball"],
     char_filter=["html_strip"]
--- api_formatter/elasticsearch/documents.py
+++ api_formatter/elasticsearch/documents.py
@@ -1,8 +1,10 @@
 # This avoids name collisions between Object class in elasticsearch_dsl and our Object Document class
 import elasticsearch_dsl as es
 
-from .analyzers import html_strip
+from .analyzers import base_analyzer
 
+
+# todo evaluate use of InnerDoc versus Nested versus Parent/Child
 
 class ExternalIdentifier(es.InnerDoc):
     source = es.Text()
@@ -29,12 +31,12 @@ class Language(es.InnerDoc):
 
 class Subnote(es.InnerDoc):
     type = es.Text()
-    content = es.Text(analyzer='snowball')
+    content = es.Text(analyzer='base_analyzer')
 
 
 class Note(es.InnerDoc):
     type = es.Text()
-    title = es.Text(analyzer='snowball', fields={'raw': es.Keyword()})
+    title = es.Text(analyzer='base_analyzer', fields={'raw': es.Keyword()})
     source = es.Text()
     subnotes = es.Object(Subnote)
 
@@ -66,12 +68,12 @@ class RightsStatement(es.InnerDoc):
 
 class Agent(es.Document):
     id = es.Text()
-    title = es.Text(analyzer='snowball', fields={'keyword': es.Keyword()})
-    description = es.Text(analyzer='snowball', fields={'keyword': es.Keyword()})
+    title = es.Text(analyzer='base_analyzer', fields={'keyword': es.Keyword()})
+    description = es.Text(analyzer='base_analyzer', fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
     dates = es.Object(Date)
     notes = es.Object(Note)
-    external_identifiers = es.Object(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier)
 
     class Index:
         name = 'agents'
@@ -79,7 +81,7 @@ class Agent(es.Document):
 
 class Collection(es.Document):
     id = es.Text()
-    title = es.Text(analyzer='snowball', fields={'keyword': es.Keyword()})
+    title = es.Text(analyzer='base_analyzer', fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
     level = es.Text(fields={'keyword': es.Keyword()})
     dates = es.Object(Date)
@@ -87,7 +89,7 @@ class Collection(es.Document):
     extents = es.Object(Extent)
     notes = es.Object(Note)
     rights_statements = es.Object(RightsStatement)
-    external_identifiers = es.Object(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier)
     agents = es.Object(URI)  # TODO: do agents need a role?
     terms = es.Object(URI)
     creators = es.Object(URI)  # TODO: should this be part of agents?
@@ -100,14 +102,14 @@ class Collection(es.Document):
 
 class Object(es.Document):
     id = es.Text()
-    title = es.Text(analyzer='snowball', fields={'keyword': es.Keyword()})
+    title = es.Text(analyzer='base_analyzer', fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
     dates = es.Object(Date)
     languages = es.Object(Language)
     extents = es.Object(Extent)
     notes = es.Object(Note)
     rights_statements = es.Object(RightsStatement)
-    external_identifiers = es.Object(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier)
     agents = es.Object(URI)  # TODO: do agents need a role?
     terms = es.Object(URI)
     ancestors = es.Object(URI)
@@ -118,9 +120,9 @@ class Object(es.Document):
 
 class Term(es.Document):
     id = es.Text()
-    title = es.Text(analyzer='snowball', fields={'keyword': es.Keyword()})
+    title = es.Text(analyzer='base_analyzer', fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
-    external_identifiers = es.Object(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier)
 
     class Index:
         name = 'terms'
