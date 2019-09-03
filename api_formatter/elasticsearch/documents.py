799bfcbb3a41e49de9fc2053638ccc84e6318362
Hillel Arnold <harnold@Hillels-MacBook-Pro.local>
2019-09-03 16:46:38 -0400

    adds docstrings, fixes #36

--- api_formatter/elasticsearch/documents.py
+++ api_formatter/elasticsearch/documents.py
@@ -5,16 +5,25 @@ from .analyzers import base_analyzer
 
 
 class ExternalIdentifier(es.InnerDoc):
+    """Abstract representation of external identifier object.
+    Used on Documents to unambiguously tie them to source data.
+    """
     source = es.Text()
     identifier = es.Text()
 
 
 class Extent(es.InnerDoc):
+    """Refers to the size of a group of records."""
     value = es.Float()
     type = es.Text()
 
 
 class Date(es.InnerDoc):
+    """Refers to a single date or date range.
+    Used on Documents to create human and machine readable date representations.
+    The `expression` field is intended to be a human readable representation of a date or date range,
+    while the begin and end values are machine readable and actionable values.
+    """
     begin = es.Date()
     end = es.Date()
     expression = es.Text()
@@ -23,16 +32,21 @@ class Date(es.InnerDoc):
 
 
 class Language(es.InnerDoc):
+    """Refers to a human language."""
     expression = es.Text()
     identifier = es.Text()
 
 
 class Subnote(es.InnerDoc):
+    """Abstract wrapper for note content, associated with Note Documents."""
     type = es.Text()
     content = es.Text(analyzer=base_analyzer)
 
 
 class Note(es.InnerDoc):
+    """Abstract representation of notes, which are typed and contain human readable
+    content in a Subnotes InnerDoc.
+    """
     type = es.Text()
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     source = es.Text()
@@ -40,13 +54,19 @@ class Note(es.InnerDoc):
 
 
 class Reference(es.InnerDoc):
+    """Abstract reference to a Document, which likely exists in a different index.
+    On initial indexing, reference may only consist of an external_identifiers array,
+    but additional data will likely be added during indexing.
+    """
     title = es.Text(analyzer=base_analyzer)
-    ref = es.Text()
-    role = es.Text()
+    uri = es.Text()
+    type = es.Text()
+    order = es.Integer()
     external_identifiers = es.Nested(ExternalIdentifier)
 
 
 class RightsGranted(es.InnerDoc):
+    """Abstract wrapper for RightsGranted information, associated with a RightsStatement Document."""
     act = es.Text()
     dateStart = es.Date()
     dateEnd = es.Date()
@@ -55,6 +75,10 @@ class RightsGranted(es.InnerDoc):
 
 
 class RightsStatement(es.InnerDoc):
+    """Machine readable representation of restrictions or permissions,
+    generally related to a group of archival records. This structure is based
+    on the PREservation Metadata: Implementation Strategies (PREMIS) Rights entity.
+    """
     determinationDate = es.Date()
     type = es.Text()
     rightsType = es.Text()
@@ -68,6 +92,9 @@ class RightsStatement(es.InnerDoc):
 
 
 class Agent(es.Document):
+    """A person, organization or family that was involved in the creation and
+    maintenance of records, or is the subject of those records.
+    """
     id = es.Text()
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     description = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
@@ -81,6 +108,12 @@ class Agent(es.Document):
 
 
 class Collection(es.Document):
+    """A group of archival records which contains other groups of records,
+    and may itself be part of a larger Collection.  Collections are not
+    physical groups of records, such as boxes and folders, but are intellectually
+    significant aggregations which crucial to understanding the context of
+    records creation and maintenance, such as record groups or series.
+    """
     id = es.Text()
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
@@ -102,10 +135,13 @@ class Collection(es.Document):
 
 
 class Object(es.Document):
+    """A group of archival records which is part of a larger Collection,
+    but does not contain any other aggregations.
+    """
     id = es.Text()
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
-    dates = es.Object(Date)
+    dates = es.Object(Date, )
     languages = es.Object(Language)
     extents = es.Nested(Extent)
     notes = es.Nested(Note)
@@ -120,6 +156,7 @@ class Object(es.Document):
 
 
 class Term(es.Document):
+    """A controlled term topical, geo"""
     id = es.Text()
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     type = es.Text(fields={'keyword': es.Keyword()})
