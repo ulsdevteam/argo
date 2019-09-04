c33165917cb64516b98fb7390f3d2523ccbf7d9c
Hillel Arnold <helrond@hotmail.com>
2019-09-03 22:46:22 -0400

    adds required fields, fixes #37

--- api_formatter/elasticsearch/documents.py
+++ api_formatter/elasticsearch/documents.py
@@ -8,14 +8,14 @@ class ExternalIdentifier(es.InnerDoc):
     """Abstract representation of external identifier object.
     Used on Documents to unambiguously tie them to source data.
     """
-    source = es.Text()
-    identifier = es.Text()
+    source = es.Text(required=True)
+    identifier = es.Text(required=True)
 
 
 class Extent(es.InnerDoc):
     """Refers to the size of a group of records."""
-    value = es.Float()
-    type = es.Text()
+    value = es.Float(required=True)
+    type = es.Text(required=True)
 
 
 class Date(es.InnerDoc):
@@ -24,33 +24,33 @@ class Date(es.InnerDoc):
     The `expression` field is intended to be a human readable representation of a date or date range,
     while the begin and end values are machine readable and actionable values.
     """
-    begin = es.Date()
+    begin = es.Date(required=True)
     end = es.Date()
-    expression = es.Text()
-    type = es.Text()
-    label = es.Text()
+    expression = es.Text(required=True)
+    type = es.Text(required=True)
+    label = es.Text(required=True)
 
 
 class Language(es.InnerDoc):
     """Refers to a human language."""
-    expression = es.Text()
-    identifier = es.Text()
+    expression = es.Text(required=True)
+    identifier = es.Text(required=True)
 
 
 class Subnote(es.InnerDoc):
     """Abstract wrapper for note content, associated with Note Documents."""
-    type = es.Text()
-    content = es.Text(analyzer=base_analyzer)
+    type = es.Text(required=True)
+    content = es.Text(required=True, analyzer=base_analyzer)
 
 
 class Note(es.InnerDoc):
     """Abstract representation of notes, which are typed and contain human readable
     content in a Subnotes InnerDoc.
     """
-    type = es.Text()
-    title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
-    source = es.Text()
-    subnotes = es.Object(Subnote)
+    type = es.Text(required=True)
+    title = es.Text(required=True, analyzer=base_analyzer, fields={'keyword': es.Keyword()})
+    source = es.Text(required=True)
+    subnotes = es.Object(Subnote, required=True)
 
 
 class Reference(es.InnerDoc):
@@ -62,16 +62,16 @@ class Reference(es.InnerDoc):
     uri = es.Text()
     type = es.Text()
     order = es.Integer()
-    external_identifiers = es.Nested(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier, required=True)
 
 
 class RightsGranted(es.InnerDoc):
     """Abstract wrapper for RightsGranted information, associated with a RightsStatement Document."""
-    act = es.Text()
-    dateStart = es.Date()
-    dateEnd = es.Date()
-    restriction = es.Text()
-    notes = es.Nested(Note)
+    act = es.Text(required=True)
+    begin = es.Date(required=True)
+    end = es.Date(required=True)
+    restriction = es.Text(required=True)
+    notes = es.Nested(Note, required=True)
 
 
 class RightsStatement(es.InnerDoc):
@@ -79,29 +79,29 @@ class RightsStatement(es.InnerDoc):
     generally related to a group of archival records. This structure is based
     on the PREservation Metadata: Implementation Strategies (PREMIS) Rights entity.
     """
-    determinationDate = es.Date()
-    type = es.Text()
-    rightsType = es.Text()
-    dateStart = es.Date()
-    dateEnd = es.Date()
-    copyrightStatus = es.Text()
-    otherBasis = es.Text()
+    determination_date = es.Date(required=True)
+    type = es.Text(required=True)
+    rights_type = es.Text(required=True)
+    begin = es.Date(required=True)
+    end = es.Date(required=True)
+    copyright_status = es.Text()
+    other_basis = es.Text()
     jurisdiction = es.Text()
     notes = es.Nested(Note)
-    rights_granted = es.Nested(RightsGranted)
+    rights_granted = es.Nested(RightsGranted, required=True)
 
 
 class Agent(es.Document):
     """A person, organization or family that was involved in the creation and
     maintenance of records, or is the subject of those records.
     """
-    id = es.Text()
-    title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
+    id = es.Text(required=True)
+    title = es.Text(required=True, analyzer=base_analyzer, fields={'keyword': es.Keyword()})
     description = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
-    type = es.Text(fields={'keyword': es.Keyword()})
+    type = es.Text(required=True, fields={'keyword': es.Keyword()})
     dates = es.Object(Date)
     notes = es.Nested(Note)
-    external_identifiers = es.Nested(ExternalIdentifier)
+    external_identifiers = es.Nested(ExternalIdentifier, required=True)
 
     class Index:
         name = 'agents'
@@ -114,21 +114,21 @@ class Collection(es.Document):
     significant aggregations which crucial to understanding the context of
     records creation and maintenance, such as record groups or series.
     """
-    id = es.Text()
+    id = es.Text(required=True)
     title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
-    type = es.Text(fields={'keyword': es.Keyword()})
+    type = es.Text(required=True, fields={'keyword': es.Keyword()})
     level = es.Text(fields={'keyword': es.Keyword()})
     dates = es.Object(Date)
     languages = es.Object(Language)
+    extents = es.Nested(Extent, required=True)
     notes = es.Nested(Note)
-    extents = es.Nested(Extent)
-    rights_statements = es.Nested(RightsStatement)
-    external_identifiers = es.Nested(ExternalIdentifier)
+    creators = es.Nested(Reference, required=True)  # TODO: should this be part of agents?
     agents = es.Nested(Reference)
     terms = es.Nested(Reference)
-    creators = es.Nested(Reference)  # TODO: should this be part of agents?
     ancestors = es.Nested(Reference)
     children = es.Nested(Reference)
+    rights_statements = es.Nested(RightsStatement)
+    external_identifiers = es.Nested(ExternalIdentifier, required=True)
 
     class Index:
         name = 'collections'
@@ -138,18 +138,18 @@ class Object(es.Document):
     """A group of archival records which is part of a larger Collection,
     but does not contain any other aggregations.
     """
-    id = es.Text()
-    title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
-    type = es.Text(fields={'keyword': es.Keyword()})
-    dates = es.Object(Date, )
+    id = es.Text(required=True)
+    title = es.Text(required=True, analyzer=base_analyzer, fields={'keyword': es.Keyword()})
+    type = es.Text(required=True, fields={'keyword': es.Keyword()})
+    dates = es.Object(Date)
     languages = es.Object(Language)
     extents = es.Nested(Extent)
     notes = es.Nested(Note)
-    rights_statements = es.Nested(RightsStatement)
-    external_identifiers = es.Nested(ExternalIdentifier)
     agents = es.Nested(Reference)
     terms = es.Nested(Reference)
     ancestors = es.Nested(Reference)
+    rights_statements = es.Nested(RightsStatement)
+    external_identifiers = es.Nested(ExternalIdentifier, required=True)
 
     class Index:
         name = 'objects'
@@ -157,10 +157,10 @@ class Object(es.Document):
 
 class Term(es.Document):
     """A controlled term topical, geo"""
-    id = es.Text()
-    title = es.Text(analyzer=base_analyzer, fields={'keyword': es.Keyword()})
-    type = es.Text(fields={'keyword': es.Keyword()})
-    external_identifiers = es.Nested(ExternalIdentifier)
+    id = es.Text(required=True)
+    title = es.Text(required=True, analyzer=base_analyzer, fields={'keyword': es.Keyword()})
+    type = es.Text(required=True, fields={'keyword': es.Keyword()})
+    external_identifiers = es.Nested(ExternalIdentifier, required=True)
 
     class Index:
         name = 'terms'
