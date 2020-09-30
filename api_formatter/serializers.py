94b7567c497c4d87f8575e44caad3fc06aae3870
Hillel Arnold <helrond@hotmail.com>
2020-09-29 23:08:44 -0400

    improving logic

--- api_formatter/serializers.py
+++ api_formatter/serializers.py
@@ -133,7 +133,6 @@ class CollectionSerializer(BaseDetailSerializer):
     agents = ReferenceSerializer(many=True, allow_null=True)
     creators = ReferenceSerializer(many=True, allow_null=True)
     terms = ReferenceSerializer(many=True, allow_null=True)
-    ancestors = ReferenceSerializer(many=True, allow_null=True)
     children = ReferenceSerializer(many=True, allow_null=True)
 
 
@@ -149,7 +148,6 @@ class ObjectSerializer(BaseDetailSerializer):
     rights_statements = RightsStatementSerializer(many=True, allow_null=True)
     agents = ReferenceSerializer(many=True, allow_null=True)
     terms = ReferenceSerializer(many=True, allow_null=True)
-    ancestors = ReferenceSerializer(many=True, allow_null=True)
 
 
 class ObjectListSerializer(BaseListSerializer):
@@ -209,15 +207,17 @@ class FacetSerializer(serializers.Serializer):
 
 
 class AncestorsSerializer(serializers.Serializer):
-    """Provides a nested dictionary representation of ancestors"""
+    """Provides a nested dictionary representation of ancestors."""
 
     def serialize_ancestors(self, ancestor_list, tree, idx):
         ancestor = ancestor_list[idx]
         serialized = ReferenceSerializer(ancestor).data
-        new_tree = {ancestor.identifier: {**serialized, **tree}}
+        tree_data = {**serialized, **tree}
         if idx == len(ancestor_list) - 1:
+            new_tree = tree_data
             return new_tree
         else:
+            new_tree = {"child": tree_data}
             return self.serialize_ancestors(ancestor_list, new_tree, idx + 1)
 
     def to_representation(self, instance):
