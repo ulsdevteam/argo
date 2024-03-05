f2a0c33b97593872e48c44d2f7acf437c0f9faf1
Tyler Lendon <trl75@pitt.edu>
2024-03-05 09:06:04 -0500

    Updated entrypoint to be slightly configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-./wait-for-it.sh db:5432 -- echo "Running entrypoint.sh"
-
 if [ ! -f manage.py ]; then
   cd argo
 fi
 
 if [ ! -f argo/config.py ]; then
-    cp argo/config.py.example argo/config.py
+    if [[ -n $PROD ]]; then
+      envsubst < argo/config.py.deploy > argo/config.py
+    else
+      cp argo/config.py.example argo/config.py
+    fi
 fi
+
+./wait-for-it.sh $db:${SQL_PORT} -- echo "Running entrypoint.sh"
 
 echo "Apply database migrations"
 python manage.py migrate
 
 #Start server
 echo "Starting server"
-python manage.py runserver 0.0.0.0:${APPLICATION_PORT}
+if [[ -n $PROD ]]; then
+    # Collect static files
+    echo "Collecting static files"
+    python manage.py collectstatic
+
+    chmod 775 /var/www/html/argo/static
+    chown www-data:www-data /var/www/html/argo/static
+    apache2ctl -D FOREGROUND
+else
+    python manage.py runserver 0.0.0.0:${APPLICATION_PORT}
+fi
